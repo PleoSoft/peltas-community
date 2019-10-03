e94660cfce4a84ef2e75af1fd989e130088477aa
Daniel Gradecak <daniel@pleosoft.com>
2019-10-03 18:04:05 +0200

    reorganizing package community/enterprise code reused

--- src/main/java/io/peltas/alfresco/workspace/AlfrescoWorkspaceApplication.java
+++ src/main/java/io/peltas/alfresco/workspace/AlfrescoWorkspaceApplication.java
@@ -18,10 +18,8 @@ package io.peltas.alfresco.workspace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class AlfrescoWorkspaceApplication {
 
 	public static void main(String[] args) throws Exception {
--- src/main/java/io/peltas/alfresco/workspace/AlfrescoWorkspaceConfiguration.java
+++ src/main/java/io/peltas/alfresco/workspace/AlfrescoWorkspaceConfiguration.java
@@ -19,35 +19,23 @@ package io.peltas.alfresco.workspace;
 import javax.sql.DataSource;
 
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.client.support.BasicAuthorizationInterceptor;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import io.peltas.core.alfresco.PeltasEntry;
-import io.peltas.core.alfresco.config.AbstractAlfrescoPeltasConfiguration;
-import io.peltas.core.alfresco.config.PeltasProperties;
-import io.peltas.core.alfresco.config.PeltasProperties.Authentication.BasicAuth;
-import io.peltas.core.batch.AbstractPeltasRestReader;
 import io.peltas.core.batch.PeltasDataHolder;
 import io.peltas.core.batch.PeltasItemWriter;
 import io.peltas.core.config.EnablePeltasInMemory;
-import io.peltas.core.repository.TxDataRepository;
-import io.peltas.core.repository.database.CustomDatasourceInitializer;
-import io.peltas.core.repository.database.CustomDatasourceProperties;
 import io.peltas.core.repository.database.JpaTxDataWriter;
 import io.peltas.core.repository.database.PeltasDatasourceInitializer;
 import io.peltas.core.repository.database.PeltasDatasourceProperties;
@@ -57,122 +45,15 @@ import io.peltas.core.repository.database.PeltasTimestampRepository;
 
 @Configuration
 @EnablePeltasInMemory
-public class AlfrescoWorkspaceConfiguration extends AbstractAlfrescoPeltasConfiguration {
-
-	@Autowired
-	private TxDataRepository dataRepository;
-
-	@Bean
-	public RestTemplate restTemplate() {
-		RestTemplate restTemplate = new RestTemplate();
-
-		BasicAuth basicAuth = alfrescoAuditProperties().getAuth().getBasic();
-		restTemplate.getInterceptors()
-				.add(new BasicAuthorizationInterceptor(basicAuth.getUsername(), basicAuth.getPassword()));
-		restTemplate.getMessageConverters().add(0, mappingJacksonHttpMessageConverter());
-		return restTemplate;
-	}
-
-	@Override
-	public AbstractPeltasRestReader<PeltasEntry, AlfrescoWorkspaceNodes> reader() {
-		RestTemplate restTemplate = restTemplate();
-
-		PeltasProperties properties = alfrescoAuditProperties();
-		return new AlfrescoWorkspaceRestReader(restTemplate, properties, dataRepository);
-	}
-
-	private MappingJackson2HttpMessageConverter mappingJacksonHttpMessageConverter() {
-		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-		converter.setObjectMapper(objectMapper());
-		return converter;
-	}
-
-	private ObjectMapper objectMapper() {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		return mapper;
-	}
-
+@EnableScheduling
+@PropertySource(ignoreResourceNotFound = true, value = { "classpath:io/peltas/peltas.properties" })
+@Import(io.peltas.core.alfresco.workspace.AlfrescoWorkspaceConfiguration.class)
+public class AlfrescoWorkspaceConfiguration {
+	
+	
 	@Configuration
-	@ConditionalOnProperty(name = "peltas.repository", havingValue = "database")
-	@EnableJpaRepositories(basePackageClasses = PeltasTimestampRepository.class)
-	@EntityScan(basePackageClasses = PeltasTimestamp.class)
-	public class DatabaseConfiguration {
-
-		@Bean
-		public JpaTxDataWriter txDataWriter(PeltasTimestampRepository repository) {
-			return new JpaTxDataWriter(repository);
-		}
-
-		@Bean
-		public PeltasJdbcBatchWriter peltasBatchWriter(JdbcTemplate jdbcTemplate,
-				@Value("classpath:io/peltas/db/executions/*.sql") Resource[] resources) {
-			return new PeltasJdbcBatchWriter(new NamedParameterJdbcTemplate(jdbcTemplate), resources);
-		}
-
-		@Bean
-		public PeltasDatasourceProperties peltasDatasourceProperties() {
-			return new PeltasDatasourceProperties();
-		}
-
-		@Bean
-		public PeltasDatasourceInitializer peltasDatasourceInitializer(DataSource dataSource,
-				ResourceLoader resourceLoader) {
-			return new PeltasDatasourceInitializer(dataSource, resourceLoader, peltasDatasourceProperties());
-		}
-		
-		@Bean
-		public PeltasDatasourceProperties peltasTimestampDatasourceProperties() {
-			PeltasDatasourceProperties datasourceProperties = new PeltasDatasourceProperties();
-			datasourceProperties.setSchema("classpath:io/peltas/db/peltas-create-timestamp-@@platform@@.sql");
-			
-			return datasourceProperties;
-		}
-
-		@Bean
-		public PeltasDatasourceInitializer peltasTimestampDatasourceInitializer(DataSource dataSource,
-				ResourceLoader resourceLoader) {
-			return new PeltasDatasourceInitializer(dataSource, resourceLoader, peltasTimestampDatasourceProperties());
-		}
-
-
-		@Bean
-		public CustomDatasourceProperties peltasCustomDatasourceProperties() {
-			return new CustomDatasourceProperties();
-		}
-
-		@Bean
-		@ConditionalOnProperty(name = "peltas.custom.datasource.enabled", havingValue = "true")
-		public CustomDatasourceInitializer peltasCustomDatasourceInitializer(DataSource dataSource,
-				ResourceLoader resourceLoader) {
-			return new CustomDatasourceInitializer(dataSource, resourceLoader, peltasCustomDatasourceProperties());
-		}
-	}
-
-	@Configuration
-	@ConditionalOnProperty(name = "peltas.repository", havingValue = "sample")
-	public class HazelcastConfiguration {
-
-		@Bean
-		public TxDataRepository txDataWriter() {
-			return new TxDataRepository() {
-
-				PeltasTimestamp lastTs = null;
-
-				@Override
-				public PeltasTimestamp readTx(String applicationName) {
-					return lastTs;
-				}
-
-				@Override
-				public PeltasTimestamp writeTx(PeltasTimestamp ts) {
-					lastTs = ts;
-					return ts;
-				}
-			};
-		}
-
+	@ConditionalOnProperty(name = "peltas.writer", havingValue = "inmemory")
+	public class InMemoryWriterConfiguration {
 		@Bean
 		public ItemWriter<PeltasDataHolder> peltasBatchWriter() {
 			return new PeltasItemWriter<Object, Object>() {
@@ -197,6 +78,55 @@ public class AlfrescoWorkspaceConfiguration extends AbstractAlfrescoPeltasConfig
 				}
 
 			};
+		}
+	}
+	
+	@Configuration
+	@ConditionalOnProperty(name = "peltas.writer", havingValue = "database")
+	public class WriterConfiguration {
+
+		@Bean
+		public PeltasJdbcBatchWriter peltasBatchWriter(JdbcTemplate jdbcTemplate,
+				@Value("classpath:io/peltas/db/executions/*.sql") Resource[] resources) {
+			return new PeltasJdbcBatchWriter(new NamedParameterJdbcTemplate(jdbcTemplate), resources);
+		}
+
+		@Bean
+		public PeltasDatasourceProperties peltasDatasourceProperties() {
+			return new PeltasDatasourceProperties();
+		}
+
+		@Bean
+		public PeltasDatasourceInitializer peltasDatasourceInitializer(DataSource dataSource,
+				ResourceLoader resourceLoader) {
+			return new PeltasDatasourceInitializer(dataSource, resourceLoader, peltasDatasourceProperties());
+		}
+	}
+
+	
+	@Configuration
+	@ConditionalOnProperty(name = "peltas.timestamp", havingValue = "database")
+	@EnableJpaRepositories(basePackageClasses = PeltasTimestampRepository.class)
+	@EntityScan(basePackageClasses = PeltasTimestamp.class)
+	public class DatabaseConfiguration {
+
+		@Bean
+		public JpaTxDataWriter txDataWriter(PeltasTimestampRepository repository) {
+			return new JpaTxDataWriter(repository);
+		}
+
+		@Bean
+		public PeltasDatasourceProperties peltasTimestampDatasourceProperties() {
+			PeltasDatasourceProperties datasourceProperties = new PeltasDatasourceProperties();
+			datasourceProperties.setSchema("classpath:io/peltas/db/peltas-create-timestamp-@@platform@@.sql");
+
+			return datasourceProperties;
+		}
+
+		@Bean
+		public PeltasDatasourceInitializer peltasTimestampDatasourceInitializer(DataSource dataSource,
+				ResourceLoader resourceLoader) {
+			return new PeltasDatasourceInitializer(dataSource, resourceLoader, peltasTimestampDatasourceProperties());
 		}
 	}
 }
